72abae7b29a41f5b231b3609dee48ab9c9472c38
Jeremy Parker <jrobinparker@gmail.com>
2018-06-06 13:48:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
 
 function sayHiToGrandma(greeting) {
   if (greeting === "HELLO") {
